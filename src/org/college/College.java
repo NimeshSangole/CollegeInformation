594f721e67e0500d92facdecdd6e3a3c5e325195
NimeshSangole <nimeshsangole007@gmail.com>
2022-05-24 10:28:50 +0530

    initial commit

---
+++ src/org/college/College.java
@@ -0,0 +1,35 @@
+package org.college;
+
+public class College {
+	public void collegeName()
+	{
+		System.out.println("sindhu mahavidyalaya");
+	}
+	public void collegeCode()
+	{
+		System.out.println("345");
+	}
+	public void collegeRank()
+	{
+		System.out.println("5");
+	}
+	public static void main(String[] args) {
+		College c=new College();
+		c.collegeName();
+		c.collegeCode();
+		c.collegeRank();
+		
+		Student s=new Student();
+		s.studentName();
+		s.studentDept();
+		s.studentId();
+		
+		Hostel h=new Hostel();
+		h.hostelName();
+		
+		Dept d=new Dept();
+		d.deptName();
+		
+	}
+
+}
---
+++ src/org/college/Student.java
@@ -0,0 +1,17 @@
+package org.college;
+
+public class Student {
+	public void studentName()
+	{
+		System.out.println("nimesh");
+	}
+	public void studentDept()
+	{
+		System.out.println("electronics");
+	}
+	public void studentId()
+	{
+		System.out.println("3456");
+	}
+
+}
